d6ae648b13b37425a7c0ad40b07cd567eabb3626
Mark Mishaev <Mark.Mishaev@checkmarx.com>
2020-02-06 19:15:36 +0200

    Add project files.

---
+++ Jaeger.CustomerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Jaeger.CustomerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly IHttpClientFactory _factory;
+
+        public CustomerController(ILogger<CustomerController> logger, IHttpClientFactory factory)
+        {
+            _logger = logger;
+            _factory = factory;
+        }
+
+        // GET api/values  
+        [HttpGet]
+        public async Task<IEnumerable<Order>> GetAsync()
+        {
+            var orders = await GetOrders();
+
+            return orders;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetAsync(int id)
+        {
+            var order = await GetOrderById(id);
+            return order;            
+        }
+
+        private async Task<Order> GetOrderById(int id)
+        {
+            var client = _factory.CreateClient("orderApi");
+
+            var requestMsg = new HttpRequestMessage(HttpMethod.Get, $"http://localhost:62686/api/orders/{id}");
+
+            var responseMsg = await client.SendAsync(requestMsg);
+
+            var orderJson = await responseMsg.Content.ReadAsStringAsync();
+
+            Order order = JsonConvert.DeserializeObject<Order>(orderJson);
+
+            return order;
+        }
+
+
+        private async Task<IEnumerable<Order>> GetOrders()
+        {
+            var client = _factory.CreateClient("orderApi");
+
+            var requestMsg = new HttpRequestMessage(HttpMethod.Get, "http://localhost:62686/api/orders");
+
+            var responseMsg = await client.SendAsync(requestMsg);
+
+            var ordersJson = await responseMsg.Content.ReadAsStringAsync();
+
+            List<Order> orders = JsonConvert.DeserializeObject<List<Order>>(ordersJson);
+
+            return orders;
+        }
+    }
+}
---
+++ Jaeger.CustomerAPI/Startup.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Jaeger.Samplers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using OpenTracing;
+using OpenTracing.Util;
+
+namespace Jaeger.CustomerAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITracer>(serviceProvider =>
+            {
+                string serviceName = Assembly.GetEntryAssembly().GetName().Name;
+
+                ILoggerFactory loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+                ISampler sampler = new ConstSampler(sample: true);
+
+                ITracer tracer = new Tracer.Builder(serviceName)
+                    .WithLoggerFactory(loggerFactory)
+                    .WithSampler(sampler)
+                    .Build();
+
+                GlobalTracer.Register(tracer);
+
+                return tracer;
+            });
+
+            services.AddHttpClient();
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customers API", Version = "v1" });
+            });
+
+            services.AddOpenTracing();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customers API V1");
+            });
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
